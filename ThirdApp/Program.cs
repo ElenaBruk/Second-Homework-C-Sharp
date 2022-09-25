e1f2041c9f5ec09f312f92803a84f427f88be394
Elena Bruk <aen_law@mail.ru>
2022-09-25 13:49:36 +0300

    Задача 15

---
+++ ThirdApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задачв 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,является ли этот день выходным.
+Console.WriteLine("Введите номер дня недели: ");
+int WeekDay = Convert.ToInt32(Console.ReadLine());
+if (WeekDay > 0 && WeekDay < 8)
+{
+    if (WeekDay == 6 || WeekDay == 7)
+    {
+        Console.WriteLine("УРАААА!!! ВЫХОДНОЙ! =)");
+    }
+    else
+    {
+        Console.WriteLine("Будний день, иди работать!");
+    }
+}
+else
+{
+    Console.WriteLine("Введен неверный номер дня недели.");
+}
